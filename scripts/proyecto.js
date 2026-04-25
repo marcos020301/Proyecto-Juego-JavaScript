177852ec5ad1b57ed8a4b6cb89b277e2e29a1f3c
Marcos <marcos2001.mgs@gmail.com>
2026-04-25 18:20:15 +0200

    Añadí selección de jugadores con stats

--- scripts/proyecto.js
+++ scripts/proyecto.js
@@ -14,3 +14,18 @@ const elemento = document.getElementById("nombreJugador");
 if (elemento) {
   elemento.textContent = nombreGuardado;
 }
+
+function seleccionarJugador(jugador) {
+  localStorage.setItem("jugadorSeleccionado", jugador);
+  window.location.href = "partida.html";
+}
+
+let jugador = localStorage.getItem("jugadorSeleccionado");
+
+if (jugador === "jugador1") {
+  console.log("Elegiste jugador 1");
+} else if (jugador === "jugador2") {
+  console.log("Elegiste jugador 2");
+} else if (jugador === "jugador3") {
+  console.log("Elegiste jugador 3");
+}
